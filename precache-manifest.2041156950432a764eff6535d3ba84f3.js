7e57a4035b4037ab076d7d25502831cd11c47535
Erko Risthein <erko@risthein.ee>
2018-11-30 14:34:47 +0200

    Updates

---
+++ precache-manifest.2041156950432a764eff6535d3ba84f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa7e1fcd6bda80f99e77",
+    "url": "/static/css/main.8922c172.chunk.css"
+  },
+  {
+    "revision": "fa7e1fcd6bda80f99e77",
+    "url": "/static/js/main.fa7e1fcd.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "71f3ecd218df7fd50bfebf9d65c52c21",
+    "url": "/index.html"
+  }
+];
